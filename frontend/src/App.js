980f1da9c303e97abe0824399e145834844ce6dd
Rodrigo Ribeiro <rodrigo.rsantos40@gmail.com>
2022-07-18 19:11:23 -0300

    inicio projeto.

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import NavBar from './Components/NavBar/navbar.js'
+
+function App() {
+  return (
+    <NavBar>
+    </NavBar>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/NavBar/navbar.js
@@ -0,0 +1,19 @@
+import icon from "../../static/icon.png";
+import "./navbar.css";
+import { useEffect, useState } from 'react';
+import M from 'materialize-css';
+
+export default function NavBaR() {
+
+    useEffect(() => {
+        M.AutoInit();
+        
+
+    }, [])
+
+    return (
+        <>
+        
+        </>
+            )
+}
