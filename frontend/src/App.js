0e57bc77b68713fb060117724b59a7145138ffd2
Rodrigo Ribeiro <rodrigo.rsantos40@gmail.com>
2022-07-19 00:18:30 -0300

    ajuste navbar, novo framework primefaces.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import NavBar from './Components/NavBar/navbar.js'
+import Footer from './Components/Footer/footer.js'
+import './index.css'
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";
+import "primereact/resources/themes/tailwind-light/theme.css"
 
 function App() {
   return (
-    <NavBar>
-    </NavBar>
+      <>
+        <NavBar>
+        </NavBar>
+          <div className='content'></div>
+        <Footer>
+        </Footer>
+      </>
   );
 }
 
--- frontend/src/Components/NavBar/navbar.js
+++ frontend/src/Components/NavBar/navbar.js
@@ -1,50 +1,28 @@
 import icon from "../../static/icon.png";
-import "./navbar.css";
 import { useEffect, useState } from 'react';
-import M from 'materialize-css';
+import { Menubar } from 'primereact/menubar';
+import "primereact/resources/themes/tailwind-light/theme.css"
+import "./navbar.css";
 
 export default function NavBaR() {
 
-    useEffect(() => {
-        M.AutoInit();
-    // Navbar
-    
-    const elemsSidenav = document.querySelectorAll(".sidenav");
-    const instancesSidenav = M.Sidenav.init(elemsSidenav, {
-        edge: "left"
-    });
-
-    }, [])
-
+    const items = [
+        {
+            label:'Sugestão',
+        },
+        {
+            label:'Sobre'
+        },
+        {
+            label:'Login'
+        },
+        {
+            label:'Contatos'
+        }
+    ];
     return (
         <>
-            <div className="navbar-fixed">
-                <nav>
-                    <div className="nav-wrapper navcor">
-
-                        <a href="#" className="brand-logo">Sanja Food</a>
-
-
-                        <a href="#" data-target="mobile-navbar" className="sidenav-trigger">
-                            <i className="material-icons">menu</i>
-                        </a>
-
-                        <ul id="navbar-items" className="right hide-on-med-and-down">
-                            <li><a href="#">Início</a></li>
-                            <li><a href="#">Sobre</a></li>
-                            <li><a href="#">Contatos</a></li>
-                            <li><a href="#">Sugestões</a></li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-
-            <ul id="mobile-navbar" className="sidenav">
-                <li><a href="#">Início</a></li>
-                <li><a href="#">Sobre</a></li>
-                <li><a href="#">Contatos</a></li>
-            </ul>
-
+        <Menubar style={{backgroundColor:'#AD8B73', border:0 ,borderRadius:0}}model={items}/>
         </>
     )
 }
